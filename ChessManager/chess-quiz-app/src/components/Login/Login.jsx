c9e184fb2cde1abb92934c38a532cc98337e7cb0
Sergio Restrepo Prado <1112040337@u.icesi.edu.co>
2025-04-03 17:07:20 -0500

    refactor: quita las clases de los componentes

--- ChessManager/chess-quiz-app/src/components/Login/Login.jsx
+++ ChessManager/chess-quiz-app/src/components/Login/Login.jsx
@@ -6,12 +6,12 @@ const Login = () => {
 
     return (
         <>
-        <div className="Login-form">
-            <div className="header-login">
+        <div>
+            <div>
                 <img src="https://raw.githubusercontent.com/SergioRP18/LOGO-ChessManager/994864e6d407751510742627ffb6c58aa3d305d5/LOGO%20AZUL.svg" alt="" />
                 <span>La estrategia, en tus manos.</span>
             </div>
-            <div className="body-login">
+            <div>
                 <form>
                     <input type="email" name="email" id="user-email" />
                     <input type="password" name="password" id="user-password" />
--- ChessManager/chess-quiz-app/src/components/Register/Register.jsx
+++ ChessManager/chess-quiz-app/src/components/Register/Register.jsx
@@ -1,22 +1,27 @@
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-
     const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Formulario enviado");
+        
+    }
 
     return (
         <>
-            <div className="register-form">
-                <div className="header-register">
+            <div>
+                <div>
                     <img src="https://raw.githubusercontent.com/SergioRP18/LOGO-ChessManager/994864e6d407751510742627ffb6c58aa3d305d5/LOGO%20AZUL.svg" alt="" />
                     <span>La estrategia, en tus manos.</span>
-                    <div className="select-rol">
+                    <div>
                         <button>Jugador</button>
                         <button>Administrador</button>
                     </div>
                 </div>
-                <div className="body-register">
-                    <form>
+                <div>
+                    <form onSubmit={handleSubmit}>
                         <input type="text" name="apodo" id="nickname" placeholder="Apodo" required/>
                         <input type="email" name="email" id="user-email" placeholder="Correo" required/>
                         <input type="password" name="contraseña" id="user-password" placeholder="Contraseña" required/>
