4634ff7edbc9a8100d990d5cf1d63d67de6e19dc
Sergio Restrepo Prado <1112040337@u.icesi.edu.co>
2025-04-07 11:43:56 -0500

    refactor: quema la data mock

--- ChessManager/chess-quiz-app/src/components/Podium/Podium.jsx
+++ ChessManager/chess-quiz-app/src/components/Podium/Podium.jsx
@@ -6,25 +6,25 @@ const Podium = ({ nickname, score }) => {
                     <h1>#2</h1>
                 </div>
                 <div>
-                    <img src="#" alt="" />
-                    <span>{nickname}</span>
-                    <span>{score}</span>
+                    <img src="https://cdn3d.iconscout.com/3d/premium/thumb/black-chess-pawn-3d-icon-download-in-png-blend-fbx-gltf-file-formats--piece-strategy-ui-vol-54-pack-user-interface-icons-5639635.png?f=webp" alt="" />
+                    <span>Luis Fernando01</span>
+                    <span>1000</span>
                 </div>
                 <div>
                     <h1>#1</h1>
                 </div>
                 <div>
-                    <img src="#" alt="" />
-                    <span>{nickname}</span>
-                    <span>{score}</span>
+                    <img src="https://images.icon-icons.com/651/PNG/512/Icon_Business_Set_00016_A_icon-icons.com_59848.png" alt="" />
+                    <span>Felipe86</span>
+                    <span>3000</span>
                 </div>
                 <div>
                     <h1>#3</h1>
                 </div>
                 <div>
-                    <img src="#" alt="" />
-                    <span>{nickname}</span>
-                    <span>{score}</span>
+                    <img src="https://i.pinimg.com/736x/5b/f7/40/5bf7403b4aae04f260c56ec19f7c4bda.jpg" alt="" />
+                    <span>Quevedo09</span>
+                    <span>2000</span>
                 </div>
             </div>
 
