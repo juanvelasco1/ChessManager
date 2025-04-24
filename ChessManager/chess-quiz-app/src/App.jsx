a39111aaa9294a18a1fd57304465b1cbc4ccc19c
Sergio Restrepo Prado <1112040337@u.icesi.edu.co>
2025-04-24 12:51:48 -0500

    fix: resuelve conexion del servidor con el store

--- ChessManager/chess-quiz-app/src/App.jsx
+++ ChessManager/chess-quiz-app/src/App.jsx
@@ -1,6 +1,6 @@
 import Router from './router'
 import {Provider} from 'react-redux'
-import {store} from './redux/store'
+import {store} from './store/store.js'
 import './App.css'
 
 function App() {
