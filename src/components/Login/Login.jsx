4a87bab72c8b06afcbd6d0625adc710e6b4834ec
Sergio Restrepo Prado <sergio.restrepo@u.icesi.edu.co>
2025-06-02 11:59:46 -0500

    Fix: corrige el ingreso del docente a la app

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -55,8 +55,14 @@ const Login = () => {
           rol: userData.role,
         }));
 
-        // Redirige según el parámetro redirect o al home
-        navigate(redirectPath);
+        // Redirige según el rol del usuario
+        if (userData.role === "profesor") {
+          navigate("/home-teacher");
+        } else if (userData.role === "jugador") {
+          navigate(redirectPath); // Redirige al lobby o al path especificado
+        } else {
+          setError("Rol no válido. Contacta al administrador.");
+        }
       } else {
         setError("El usuario no existe en la base de datos.");
       }
