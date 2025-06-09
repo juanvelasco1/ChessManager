ee0bb4c22fcd4b63a91f0d08277871f3349d6443
JuanVB1 <131167099+juanvelasco1@users.noreply.github.com>
2025-06-09 15:49:22 -0500

    StylesFinal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
         position: "fixed",
         top: {
           xs: 690,     // mobile
-          md: 850           // desktop
+          md: 930           // desktop
         },
         bottom: {
           xs: 50,          // mobile
